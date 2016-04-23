fc2a95c4231780af1a04005a479f14b6009d4cbb
tyhjh <1043315346@qq.com>
2016-04-23 12:09:56 +0800

    定时功能

---
+++ Smart/app/src/main/java/com/example/tyhj/smart/AddControlCenter.java
@@ -0,0 +1,16 @@
+package com.example.tyhj.smart;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by Tyhj on 2016/4/23.
+ */
+public class AddControlCenter extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.addcontrolcenter);
+    }
+}
